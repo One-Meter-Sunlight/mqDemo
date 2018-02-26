d273385732f045282c0e16e87e7c3118befe4c9c
ck9527 <chenk@ubox.cn>
2018-02-26 10:24:07 +0800

    Initial commit

---
+++ src/main/java/com/iccom/p2p/Provider.java
@@ -0,0 +1,56 @@
+package com.iccom.p2p;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQTextMessage;
+
+import javax.jms.*;
+
+import static javax.jms.Session.AUTO_ACKNOWLEDGE;
+
+/**
+ * 生产者
+ * <p>
+ * Create by CK on 2018/2/26 9:50
+ */
+public class Provider {
+
+    private static final String URL = "tcp://127.0.0.1:61616";
+    private static final String QUEUE_NAME = "testQueue";
+
+    public void sendMsg() throws JMSException {
+        // 获得连接工厂，由JMS规范提供
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(URL);
+
+        // 获得连接
+        Connection connection = connectionFactory.createConnection();
+
+        // 启动
+        connection.start();
+
+        // 获得Session，不设置事务，为true时，第二个参数忽略，现在设置自动
+        Session session = connection.createSession(false, AUTO_ACKNOWLEDGE);
+
+        // 创建目标
+        Destination destination = session.createQueue(QUEUE_NAME);
+
+        // 生产者
+        MessageProducer messageProducer = session.createProducer(destination);
+
+        for (int i = 0; i < 50; i++) {
+            TextMessage textMessage = new ActiveMQTextMessage();
+            String str = "发送的队列消息为：test" + i;
+            textMessage.setText(str);
+            System.out.println(str);
+            messageProducer.send(textMessage);
+        }
+
+        // 关闭连接
+        connection.close();
+    }
+
+    public static void main(String[] args) throws JMSException {
+        Provider provider = new Provider();
+        provider.sendMsg();
+    }
+
+}
